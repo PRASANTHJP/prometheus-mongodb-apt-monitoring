84d7567e12ce068d618f3a308ed674d977947595
Prasanth J <14804266+PRASANTHJP@users.noreply.github.com>
2024-09-26 11:15:22 +0530

    Create node_exporter.sh

---
+++ exporters/node_exporter.sh
@@ -0,0 +1,11 @@
+#A shell script to install and run Node Exporter
+
+#!/bin/bash
+
+# Download Node Exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+tar xvfz node_exporter-1.5.0.linux-amd64.tar.gz
+sudo cp node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin/
+
+# Start Node Exporter
+sudo node_exporter &
